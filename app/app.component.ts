6d2e7587680322d52b67bfa86a2d6a3e80d4cb78
CodeStrider35 <prince1moreno@gmail.com>
2016-05-06 15:15:24 -0700

    first commit

---
+++ app/app.component.ts
@@ -0,0 +1,12 @@
+import {Component} from 'angular2/core';
+import {CoursesComponent} from './courses.component'
+import {AuthorsComponent} from './authors.component'
+
+@Component({
+    selector: 'my-app',
+    template: `<h1 class="md-display-3">Hello Angular 2</h1>
+               <courses></courses>
+               <authors></authors>`
+    directives: [CoursesComponent, AuthorsComponent]
+})
+export class AppComponent { }
